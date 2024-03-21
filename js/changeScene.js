0508e749d2eb614625cd4c4b1b8504b8c19d3acf
JIN <jinorabel@gmail.com>
2024-03-21 11:54:14 +0900

    feat : drag and drop

--- js/changeScene.js
+++ js/changeScene.js
@@ -1,33 +1,35 @@
 import { fetchJsonAndDisplayData } from './fetch.js';
 
 function changeScene() {
-  const sceneNum = 3; // 원하는 scene 숫자를 여기에 설정
+  const sceneNum = 2; // 원하는 scene 숫자를 여기에 설정
   fetchJsonAndDisplayData(sceneNum);
   changeImg(sceneNum);
 }
 
 // 이미지 바꾸는 함수
 function changeImg(sceneNum) {
-  const imgSrcLeft = document.getElementById('left').src;
-  const imgSrcRight = document.getElementById('right').src;
+  let imgLeft = document.querySelector('.left img');
+  let imgRight = document.querySelector('.right img');
 
-  imgSrcLeft = '';
-  imgSrcRight = '';
+  imgLeft.src = '';
+  imgRight.src = '';
 
   switch (sceneNum) {
     case 1:
-      imgSrcLeft = './img/pig1_straw.png';
-      imgSrcRight = '';
+      imgLeft.src = './img/pig1_straw.png';
       break;
     case 2:
+      // 이미지 변경 로직 추가
       break;
     case 3:
-      imgSrcLeft = './img/pig1_straw.png';
-      imgSrcRight = './img/house_straw';
+      imgLeft.src = './img/pig1_straw.png';
+      imgRight.src = './img/house_straw.png';
       break;
     case 4:
+      // 이미지 변경 로직 추가
       break;
     case 5:
+      // 이미지 변경 로직 추가
       break;
 
     default:
